189f84872edff8d11f796f563cf75179231d54f4
Jonathan Martin <me@nybblr.com>
2014-03-15 17:21:14 -0400

    Initial scaffold and gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReallyApi::Application.config.secret_key_base = '749da670920a82c7db3b72e83d70b4e9ed7289cbbeb50e0256a049733238f8e006fbc88610999d5e8e40654937f520b5f49ec7d6a06ff8d5b26f783b5392bb6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReallyApi::Application.config.session_store :cookie_store, key: '_really_api_session'
